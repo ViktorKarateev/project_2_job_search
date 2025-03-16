3e0838b5ae0508333b71f90fde3c5a29109e2c47
Karateev <karateev.92@mail.ru>
2025-03-16 11:19:10 +0400

    Добавлен абстрактный класс APIHandler

---
+++ src/base_api.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+
+class APIHandler(ABC):
+    """
+    Абстрактный класс для API-сервисов по вакансиям.
+    Все наследники должны реализовать метод get_vacancies.
+    """
+
+    @abstractmethod
+    def get_vacancies(self, keyword: str) -> List[Dict]:
+        """
+        Метод для получения списка вакансий по ключевому слову.
+        Должен быть реализован в наследниках.
+        :param keyword: Ключевое слово для поиска вакансий
+        :return: Список словарей с вакансиями
+        """
+        pass
